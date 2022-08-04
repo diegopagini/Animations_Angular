10cffe4c8ebd425c0ba6669153c97b9f1f47e0e3
Diego Pagini <diegopaginidev@gmail.com>
2022-08-04 10:27:50 +0200

    simple anitamion

--- src/app/simple/simple.component.ts
+++ src/app/simple/simple.component.ts
@@ -1,8 +1,29 @@
+import { state, style, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-simple',
   templateUrl: './simple.component.html',
   styleUrls: ['./simple.component.scss'],
+  animations: [
+    trigger('clickedState', [
+      state(
+        'default',
+        style({
+          'background-color': 'orange',
+          height: '100px', // If we don't put "px" it will be percentage: like this --> height: 100
+          width: '100px',
+        })
+      ), // Can use 'background-color' or backgroundColor
+      state(
+        'clicked',
+        style({
+          backgroundColor: 'blue',
+          height: '250px',
+          width: '250px',
+        })
+      ),
+    ]),
+  ],
 })
 export class SimpleComponent {}
